2e85ce5a66f8dfed80a49baeea3a9ac05ddf2710
Rohan <31851961+rohanthakran@users.noreply.github.com>
2020-03-11 16:31:56 +0530

    Update style.js

--- style.js
+++ style.js
@@ -1,5 +1,23 @@
 function getHistory(){
-    return document.getElementById("history-value").innerText;
+    return document.getElementById("history-value").innerHTML;
 
 }
-console.log(getHistory());
+
+function printHistroy(num){
+         document.getElementById("history-value").innerHTML = num;
+}       
+printHistroy("9-98");
+
+function getOutput(num){
+    document.getElementById("output-value").innerHTML = num;
+}
+function printOutput(num){
+    document.getElementById("output-value").innerHTML = getFormatNUmber(num);
+}
+
+function getFormatNUmber(num){
+    var n = Number(num);
+    var value = n.toLocaleString("en");
+    return value;
+}
+printOutput("9999");
